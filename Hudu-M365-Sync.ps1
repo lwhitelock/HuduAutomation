7ab4bdc53c10a84d9598077c772149e33bdb613e
lwhitelock <79275328+lwhitelock@users.noreply.github.com>
2021-04-29 23:18:56 +0100

    Update Hudu-M365-Sync.ps1

--- Hudu-M365-Sync.ps1
+++ Hudu-M365-Sync.ps1
@@ -37,6 +37,132 @@ if (Get-Module -ListAvailable -Name HuduAPI) {
 		Import-Module HuduAPI
 	}
 
+
+####### License Lookup Hash #########
+$LicenseLookup = @{
+'SPZA_IW' = 'App Connect Iw'
+'AAD_BASIC' = 'Azure Active Directory Basic'
+'AAD_PREMIUM' = 'Azure Active Directory Premium P1'
+'AAD_PREMIUM_P2' = 'Azure Active Directory Premium P2'
+'RIGHTSMANAGEMENT' = 'Azure Information Protection Plan 1'
+'MCOCAP' = 'Common Area Phone'
+'MCOPSTNC' = 'Communications Credits'
+'DYN365_ENTERPRISE_PLAN1' = 'Dynamics 365 Customer Engagement Plan Enterprise Edition'
+'DYN365_ENTERPRISE_CUSTOMER_SERVICE' = 'Dynamics 365 For Customer Service Enterprise Edition'
+'DYN365_FINANCIALS_BUSINESS_SKU' = 'Dynamics 365 For Financials Business Edition'
+'DYN365_ENTERPRISE_SALES_CUSTOMERSERVICE' = 'Dynamics 365 For Sales And Customer Service Enterprise Edition'
+'DYN365_ENTERPRISE_SALES' = 'Dynamics 365 For Sales Enterprise Edition'
+'DYN365_ENTERPRISE_TEAM_MEMBERS' = 'Dynamics 365 For Team Members Enterprise Edition'
+'DYN365_TEAM_MEMBERS' = 'Dynamics 365 Team Members'
+'Dynamics_365_for_Operations' = 'Dynamics 365 Unf Ops Plan Ent Edition'
+'EMS' = 'Enterprise Mobility + Security E3'
+'EMSPREMIUM' = 'Enterprise Mobility + Security E5'
+'EXCHANGESTANDARD' = 'Exchange Online (Plan 1)'
+'EXCHANGEENTERPRISE' = 'Exchange Online (Plan 2)'
+'EXCHANGEARCHIVE_ADDON' = 'Exchange Online Archiving For Exchange Online'
+'EXCHANGEARCHIVE' = 'Exchange Online Archiving For Exchange Server'
+'EXCHANGEESSENTIALS' = 'Exchange Online Essentials'
+'EXCHANGE_S_ESSENTIALS' = 'Exchange Online Essentials'
+'EXCHANGEDESKLESS' = 'Exchange Online Kiosk'
+'EXCHANGETELCO' = 'Exchange Online Pop'
+'INTUNE_A' = 'Intune'
+'M365EDU_A1' = 'Microsoft 365 A1'
+'M365EDU_A3_FACULTY' = 'Microsoft 365 A3 For Faculty'
+'M365EDU_A3_STUDENT' = 'Microsoft 365 A3 For Students'
+'M365EDU_A5_FACULTY' = 'Microsoft 365 A5 For Faculty'
+'M365EDU_A5_STUDENT' = 'Microsoft 365 A5 For Students'
+'O365_BUSINESS' = 'Microsoft 365 Apps For Business'
+'SMB_BUSINESS' = 'Microsoft 365 Apps For Business'
+'OFFICESUBSCRIPTION' = 'Microsoft 365 Apps For Enterprise'
+'MCOMEETADV' = 'Microsoft 365 Audio Conferencing'
+'MCOMEETADV_GOC' = 'Microsoft 365 Audio Conferencing For Gcc'
+'O365_BUSINESS_ESSENTIALS' = 'Microsoft 365 Business Basic'
+'SMB_BUSINESS_ESSENTIALS' = 'Microsoft 365 Business Basic'
+'SPB' = 'Microsoft 365 Business Premium'
+'O365_BUSINESS_PREMIUM' = 'Microsoft 365 Business Standard'
+'SMB_BUSINESS_PREMIUM' = 'Microsoft 365 Business Standard'
+'MCOPSTN_5' = 'Microsoft 365 Domestic Calling Plan (120 Minutes)'
+'SPE_E3' = 'Microsoft 365 E3'
+'SPE_E3_USGOV_DOD' = 'Microsoft 365 E3_Usgov_Dod'
+'SPE_E3_USGOV_GCCHIGH' = 'Microsoft 365 E3_Usgov_Gcchigh'
+'SPE_E5' = 'Microsoft 365 E5'
+'INFORMATION_PROTECTION_COMPLIANCE' = 'Microsoft 365 E5 Compliance'
+'IDENTITY_THREAT_PROTECTION' = 'Microsoft 365 E5 Security'
+'IDENTITY_THREAT_PROTECTION_FOR_EMS_E5' = 'Microsoft 365 E5 Security For Ems E5'
+'M365_F1' = 'Microsoft 365 F1'
+'SPE_F1' = 'Microsoft 365 F3'
+'M365_G3_GOV' = 'Microsoft 365 Gcc G3'
+'MCOEV' = 'Microsoft 365 Phone System'
+'PHONESYSTEM_VIRTUALUSER' = 'Microsoft 365 Phone System - Virtual User'
+'MCOEV_DOD' = 'Microsoft 365 Phone System For Dod'
+'MCOEV_FACULTY' = 'Microsoft 365 Phone System For Faculty'
+'MCOEV_GOV' = 'Microsoft 365 Phone System For Gcc'
+'MCOEV_GCCHIGH' = 'Microsoft 365 Phone System For Gcchigh'
+'MCOEVSMB_1' = 'Microsoft 365 Phone System For Small And Medium Business'
+'MCOEV_STUDENT' = 'Microsoft 365 Phone System For Students'
+'MCOEV_TELSTRA' = 'Microsoft 365 Phone System For Telstra'
+'MCOEV_USGOV_DOD' = 'Microsoft 365 Phone System_Usgov_Dod'
+'MCOEV_USGOV_GCCHIGH' = 'Microsoft 365 Phone System_Usgov_Gcchigh'
+'WIN_DEF_ATP' = 'Microsoft Defender Advanced Threat Protection'
+'CRMSTANDARD' = 'Microsoft Dynamics Crm Online'
+'CRMPLAN2' = 'Microsoft Dynamics Crm Online Basic'
+'FLOW_FREE' = 'Microsoft Flow Free'
+'INTUNE_A_D_GOV' = 'Microsoft Intune Device For Government'
+'POWERAPPS_VIRAL' = 'Microsoft Power Apps Plan 2 Trial'
+'TEAMS_FREE' = 'Microsoft Team (Free)'
+'TEAMS_EXPLORATORY' = 'Microsoft Teams Exploratory'
+'IT_ACADEMY_AD' = 'Ms Imagine Academy'
+'ENTERPRISEPREMIUM_FACULTY' = 'Office 365 A5 For Faculty'
+'ENTERPRISEPREMIUM_STUDENT' = 'Office 365 A5 For Students'
+'EQUIVIO_ANALYTICS' = 'Office 365 Advanced Compliance'
+'ATP_ENTERPRISE' = 'Office 365 Advanced Threat Protection (Plan 1)'
+'STANDARDPACK' = 'Office 365 E1'
+'STANDARDWOFFPACK' = 'Office 365 E2'
+'ENTERPRISEPACK' = 'Office 365 E3'
+'DEVELOPERPACK' = 'Office 365 E3 Developer'
+'ENTERPRISEPACK_USGOV_DOD' = 'Office 365 E3_Usgov_Dod'
+'ENTERPRISEPACK_USGOV_GCCHIGH' = 'Office 365 E3_Usgov_Gcchigh'
+'ENTERPRISEWITHSCAL' = 'Office 365 E4'
+'ENTERPRISEPREMIUM' = 'Office 365 E5'
+'ENTERPRISEPREMIUM_NOPSTNCONF' = 'Office 365 E5 Without Audio Conferencing'
+'DESKLESSPACK' = 'Office 365 F3'
+'ENTERPRISEPACK_GOV' = 'Office 365 Gcc G3'
+'MIDSIZEPACK' = 'Office 365 Midsize Business'
+'LITEPACK' = 'Office 365 Small Business'
+'LITEPACK_P2' = 'Office 365 Small Business Premium'
+'WACONEDRIVESTANDARD' = 'Onedrive For Business (Plan 1)'
+'WACONEDRIVEENTERPRISE' = 'Onedrive For Business (Plan 2)'
+'POWER_BI_STANDARD' = 'Power Bi (Free)'
+'POWER_BI_ADDON' = 'Power Bi For Office 365 Add-On'
+'POWER_BI_PRO' = 'Power Bi Pro'
+'PROJECTCLIENT' = 'Project For Office 365'
+'PROJECTESSENTIALS' = 'Project Online Essentials'
+'PROJECTPREMIUM' = 'Project Online Premium'
+'PROJECTONLINE_PLAN_1' = 'Project Online Premium Without Project Client'
+'PROJECTPROFESSIONAL' = 'Project Online Professional'
+'PROJECTONLINE_PLAN_2' = 'Project Online With Project For Office 365'
+'SHAREPOINTSTANDARD' = 'Sharepoint Online (Plan 1)'
+'SHAREPOINTENTERPRISE' = 'Sharepoint Online (Plan 2)'
+'MCOIMP' = 'Skype For Business Online (Plan 1)'
+'MCOSTANDARD' = 'Skype For Business Online (Plan 2)'
+'MCOPSTN2' = 'Skype For Business Pstn Domestic And International Calling'
+'MCOPSTN1' = 'Skype For Business Pstn Domestic Calling'
+'MCOPSTN5' = 'Skype For Business Pstn Domestic Calling (120 Minutes)'
+'MCOPSTNEAU2' = 'Telstra Calling For O365'
+'TOPIC_EXPERIENCES' = 'Topic Experiences'
+'VISIOONLINE_PLAN1' = 'Visio Online Plan 1'
+'VISIOCLIENT' = 'Visio Online Plan 2'
+'VISIOCLIENT_GOV' = 'Visio Plan 2 For Gov'
+'WIN10_PRO_ENT_SUB' = 'Windows 10 Enterprise E3'
+'WIN10_VDA_E3' = 'Windows 10 Enterprise E3'
+'WIN10_VDA_E5' = 'Windows 10 Enterprise E5'
+'WINDOWS_STORE' = 'Windows Store For Business'
+'RMSBASIC' = 'Azure Information Protection Basic'
+}
+
+
+
+
 #Login to Hudu
 New-HuduAPIKey $HuduAPIKey
 New-HuduBaseUrl $HuduBaseDomain
@@ -61,16 +187,20 @@ foreach ($customer in $customers) {
 	if ($($hududomain.id.count) -gt 0) {
 		
 		#Create a table to send into Hudu
-		$CustomerLinks = "<table style=`"width:400px; border: 1px solid black;`">	 
-        <tr><td><i class=`"fas fa-cogs`">&nbsp;&nbsp;&nbsp;</i><a target=`"_blank`" href=`"https://portal.office.com/Partner/BeginClientSession.aspx?CTID=$($customer.TenantId)&CSDEST=o365admincenter`">M365 Admin Portal</a></td>
-        <td><i class=`"fas fa-mail-bulk`">&nbsp;&nbsp;&nbsp;</i><a target=`"_blank`" href=`"https://outlook.office365.com/ecp/?rfr=Admin_o365&exsvurl=1&delegatedOrg=$($Customer.DefaultDomainName)`">Exchange Admin Portal</a></td></tr>
-        <tr><td><i class=`"fas fa-users-cog`">&nbsp;&nbsp;&nbsp;</i><a target=`"_blank`" href=`"https://aad.portal.azure.com/$($Customer.DefaultDomainName)`" >Azure Active Directory</a></td>
-        <td><i class=`"fas fa-key`">&nbsp;&nbsp;&nbsp;</i><a target=`"_blank`" href=`"https://account.activedirectory.windowsazure.com/usermanagement/multifactorverification.aspx?tenantId=$($Customer.tenantid)&culture=en-us&requestInitiatedContext=users`" >MFA Portal (Read Only)</a></td></tr>
-        <tr><td><i class=`"fab fa-skype`">&nbsp;&nbsp;&nbsp;</i><a target=`"_blank`" href=`"https://portal.office.com/Partner/BeginClientSession.aspx?CTID=$($Customer.TenantId)&CSDEST=MicrosoftCommunicationsOnline`">Sfb Portal</a></td>
-        <td><i class=`"fas fa-users`">&nbsp;&nbsp;&nbsp;</i><a target=`"_blank`" href=`"https://admin.teams.microsoft.com/?delegatedOrg=$($Customer.DefaultDomainName)`">Teams Portal</a></td></tr>
-        <tr><td><i class=`"fas fa-server`">&nbsp;&nbsp;&nbsp;</i><a target=`"_blank`" href=`"https://portal.azure.com/$($customer.DefaultDomainName)`">Azure Portal</a></td>
-        <td><i class=`"fas fa-laptop`">&nbsp;&nbsp;&nbsp;</i><a target=`"_blank`" href=`"https://endpoint.microsoft.com/$($customer.DefaultDomainName)/`">Endpoint Management</a></td></tr>
-		</table>"
+		$CustomerLinks = "<div class=`"nasa__content`"> 
+        <div class=`"nasa__block`"><button class=`"button`"><h2><a target=`"_blank`" href=`"https://portal.office.com/Partner/BeginClientSession.aspx?CTID=$($customer.TenantId)&CSDEST=o365admincenter`"><i class=`"fas fa-cogs`">&nbsp;&nbsp;&nbsp;</i>M365 Admin Portal</a></h2></button></div>
+        <div class=`"nasa__block`"><button class=`"button`"><h2><a target=`"_blank`" href=`"https://outlook.office365.com/ecp/?rfr=Admin_o365&exsvurl=1&delegatedOrg=$($Customer.DefaultDomainName)`"><i class=`"fas fa-mail-bulk`">&nbsp;&nbsp;&nbsp;</i>Exchange Admin Portal</a></h2></button></div>
+        <div class=`"nasa__block`"><button class=`"button`"><h2><a target=`"_blank`" href=`"https://aad.portal.azure.com/$($Customer.DefaultDomainName)`" ><i class=`"fas fa-users-cog`">&nbsp;&nbsp;&nbsp;</i>Azure Active Directory</a></h2></button></div>
+		<div class=`"nasa__block`"><button class=`"button`"><h2><a target=`"_blank`" href=`"https://endpoint.microsoft.com/$($customer.DefaultDomainName)/`"><i class=`"fas fa-laptop`">&nbsp;&nbsp;&nbsp;</i>Endpoint Management</a></h2></button></td></div>
+        
+
+
+		<div class=`"nasa__block`"><button class=`"button`"><h2><a target=`"_blank`" href=`"https://portal.office.com/Partner/BeginClientSession.aspx?CTID=$($Customer.TenantId)&CSDEST=MicrosoftCommunicationsOnline`"><i class=`"fab fa-skype`">&nbsp;&nbsp;&nbsp;</i>Sfb Portal</a></h2></button></div>
+        <div class=`"nasa__block`"><button class=`"button`"><h2><a target=`"_blank`" href=`"https://admin.teams.microsoft.com/?delegatedOrg=$($Customer.DefaultDomainName)`"><i class=`"fas fa-users`">&nbsp;&nbsp;&nbsp;</i>Teams Portal</a></h2></button></div>
+        <div class=`"nasa__block`"><button class=`"button`"><h2><a target=`"_blank`" href=`"https://portal.azure.com/$($customer.DefaultDomainName)`"><i class=`"fas fa-server`">&nbsp;&nbsp;&nbsp;</i>Azure Portal</a></h2></button></div>
+        <div class=`"nasa__block`"><button class=`"button`"><h2><a target=`"_blank`" href=`"https://account.activedirectory.windowsazure.com/usermanagement/multifactorverification.aspx?tenantId=$($Customer.tenantid)&culture=en-us&requestInitiatedContext=users`" ><i class=`"fas fa-key`">&nbsp;&nbsp;&nbsp;</i>MFA Portal (Read Only)</a></h2></button></div>
+		
+		</div>"
 	
 		#Grab a count of licensed users so we have something to show on the badge		
 		$msusers = Get-MsolUser -TenantID $customer.tenantid -All | where {$_.isLicensed -eq $true}
@@ -81,21 +211,56 @@ foreach ($customer in $customers) {
 		#Grab extra info to put into Hudu
         $companyInfo = Get-MsolCompanyInformation -TenantId $customer.TenantId
 		$customerDomains = (Get-MsolDomain -TenantId $customer.tenantid | Where-Object {$_.status -contains "Verified"}).Name -join ', ' | Out-String
-		$detailstable = "<table style=`"width:600px; border: 1px solid black;`"><tr><td>Tenant Name</td><td>$($customer.Name)</td></tr>
-						<tr><td>Tenant ID</td><td>$($customer.TenantId)</td></tr>
-						<tr><td>Default Domain</td><td>$defaultdomain</td></tr>
-						<tr><td>Customer Domains</td><td>$customerDomains</td></tr>
-						</table>"
+		$detailstable = "<div class='nasa__block'>
+							<header class='nasa__block-header'>
+							<h1><i class='fas fa-info-circle icon'></i>Basic Info</h1>
+							 </header>
+								<main>
+								<article>
+								<div class='basic_info__section'>
+								<h2>Tenant Name</h2>
+								<p>
+									$($customer.Name)
+								</p>
+								</div>
+								<div class='basic_info__section'>
+								<h2>Tenant ID</h2>
+								<p>
+									$($customer.TenantId)
+								</p>
+								</div>
+								<div class='basic_info__section'>
+								<h2>Default Domain</h2>
+								<p>
+									$defaultdomain
+								</p>
+								</div>
+								<div class='basic_info__section'>
+								<h2>Customer Domains</h2>
+								<p>
+									$customerDomains
+								</p>
+								</div>
+						</article>
+						</main>
+						</div>
+"
 		
 		
 		$Licenses = $null
 		$Licenses = Get-MsolAccountSku -TenantId $customer.TenantId
 		if ($Licenses) {
-        $licenseTableTop = "<br/><table style=`"width:600px; border: 1px solid black;`"><thead><tr><th>License Name</th><th>Active</th><th>Consumed</th><th>Unused</th></tr></thead><tbody><tr><td>"
-        $licenseTableBottom = "</td></tr></tbody></table>"
+        $licenseTableTop = "<div class=`"nasa__block`"><header class='nasa__block-header'>
+							<h1><i class='fas fa-info-circle icon'></i>Current Licenses</h1>
+							 </header><table><thead><tr><th>License Name</th><th>Active</th><th>Consumed</th><th>Unused</th></tr></thead><tbody><tr><td>"
+        $licenseTableBottom = "</td></tr></tbody></table></div>"
         $licensesColl = @()
         foreach ($license in $licenses) {
-            $licenseString = "$($license.SkuPartNumber)</td><td>$($license.ActiveUnits) active</td><td>$($license.ConsumedUnits) consumed</td><td>$($license.ActiveUnits - $license.ConsumedUnits) unused"
+			$LicenseName = $LicenseLookup."$($license.SkuPartNumber)"
+			if (!$LicenseName) {
+			$LicenseName = $license.SkuPartNumber
+			}
+            $licenseString = "$LicenseName</td><td>$($license.ActiveUnits) active</td><td>$($license.ConsumedUnits) consumed</td><td>$($license.ActiveUnits - $license.ConsumedUnits) unused"
             $licensesColl += $licenseString
         }
         if ($licensesColl) {
@@ -108,12 +273,22 @@ foreach ($customer in $customers) {
 		$licensedUserTable = $null
 		$licensedUsers = get-msoluser -TenantId $customer.TenantId -All | Where-Object {$_.islicensed} | Sort-Object UserPrincipalName
 		if ($licensedUsers) {
-			$licensedUsersTableTop = "<br/><table style=`"width:80%; border: 1px solid black;`"><thead><tr><th>Display Name</th><th>Addresses</th><th>Assigned Licenses</th></tr></thead><tbody><tr><td>"
-			$licensedUsersTableBottom = "</td></tr></tbody></table>"
+			$licensedUsersTableTop = "<div class=`"nasa__block`"><header class='nasa__block-header'>
+							<h1><i class='fas fa-users icon'></i>Licensed Users</h1>
+							 </header><table><thead><tr><th>Display Name</th><th>Addresses</th><th>Assigned Licenses</th><th>Options</th></tr></thead><tbody><tr><td>"
+			$licensedUsersTableBottom = "</td></tr></tbody></table></div>"
 			$licensedUserColl = @()
 			foreach ($user in $licensedUsers) {
              	$aliases = (($user.ProxyAddresses | Where-Object {$_ -cnotmatch "SMTP" -and $_ -notmatch ".onmicrosoft.com"}) -replace "SMTP:", " ") -join "<br/>"
-				$licensedUserString = "$($user.DisplayName)</td><td><strong>$($user.UserPrincipalName)</strong><br/>$aliases</td><td>$(($user.Licenses.accountsku.skupartnumber) -join "<br/>")"
+				$userLicenses = $user.Licenses.accountsku.skupartnumber | foreach { 
+				$lookedUP = $LicenseLookup.$_ 
+				if ($lookedUp){
+					"$LookedUp <br />"
+					} Else {
+					"$_ <br />"
+					}
+				}
+				$licensedUserString = "$($user.DisplayName)</td><td><strong>$($user.UserPrincipalName)</strong><br/>$aliases</td><td>$userLicenses</td><td><a target=`"_blank`" href=https://aad.portal.azure.com/$($Customer.DefaultDomainName)/#blade/Microsoft_AAD_IAM/UserDetailsMenuBlade/Profile/userId/$($user.ObjectId)>View</a>"
 				$licensedUserColl += $licensedUserString
 			}
 			if ($licensedUserColl) {
@@ -124,9 +299,14 @@ foreach ($customer in $customers) {
 	
 	#Build the output
 	$body = "<div class=`"nasa-block`"><h2>Administration Portals</h2> $CustomerLinks</div> 
-			 <div class=`"nasa-block`"><h2>Tenant Details</h2> $detailstable</div>
-			 <div class=`"nasa-block`"><h2>Current Licenses</h2> $licenseTable</div>
-			 <div class=`"nasa-block`"><h2>Licensed Users</h2> $licensedUserTable</div>"
+			<br />
+			 <div class=`"nasa__content`">
+			 $detailstable
+			 $licenseTable
+			 </div>
+			 <div class=`"nasa__content`">
+			 $licensedUserTable
+			 </div>"
       
    	
 	$result = Set-HuduMagicDash -title "Microsoft 365 - $($hududomain[0].company_name)" -company_name $company_name -message "$($msusers.count) Licensed Users" -icon "fab fa-microsoft" -content $body -shade "success"	
@@ -158,7 +338,3 @@ foreach ($customer in $customers) {
 	}
 	}
 }	
-
-      
-
-  
