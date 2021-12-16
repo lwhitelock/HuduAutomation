77c81924fa7ea023211de735004563f27c927e44
lwhitelock <79275328+lwhitelock@users.noreply.github.com>
2021-12-16 11:52:37 +0000

    Update Hudu-LAPSAlternative-Documentation.ps1

--- CyberdrainRewrite/Hudu-LAPSAlternative-Documentation.ps1
+++ CyberdrainRewrite/Hudu-LAPSAlternative-Documentation.ps1
@@ -12,67 +12,70 @@ $ChangeAdminUsername = $true
 
 
 if (Get-Module -ListAvailable -Name HuduAPI) {
-		Import-Module HuduAPI 
-	} else {
-		Install-Module HuduAPI -Force
-		Import-Module HuduAPI
-	}
+    Import-Module HuduAPI 
+} else {
+    Install-Module HuduAPI -Force
+    Import-Module HuduAPI
+}
   
 #Set Hudu logon information
 New-HuduAPIKey $HuduAPIKey
 New-HuduBaseUrl $HuduBaseDomain
 
-		$Company = Get-HuduCompanies -name $CompanyName
-		if ($company) {	
-			#This is the data we'll be sending to Hudu
+$Company = Get-HuduCompanies -name $CompanyName
+if ($company) {	
+    #This is the data we'll be sending to Hudu
 		
-			$ComputerName = $($Env:COMPUTERNAME)
+    $ComputerName = $($Env:COMPUTERNAME)
 			
-			#Find the parent asset from serial
-			$ParentAsset = Get-HuduAssets -primary_serial (get-ciminstance win32_bios).serialnumber
+    #Find the parent asset from serial
+    $ParentAsset = Get-HuduAssets -primary_serial (get-ciminstance win32_bios).serialnumber
 			
-			#If count exists we either got 0 or more than 1 either way lets try to match off name
-			if ($ParentAsset.count){
-				$ParentAsset = Get-HuduAssets -companyid $company.id -name $ComputerName
-			}
-		
-		add-type -AssemblyName System.Web
-		#This is the process we'll be perfoming to set the admin account.
-		$LocalAdminPassword = [System.Web.Security.Membership]::GeneratePassword(24,5)
-		If($ChangeAdminUsername -eq $false) {
-		Set-LocalUser -name "Administrator" -Password ($LocalAdminPassword | ConvertTo-SecureString -AsPlainText -Force) -PasswordNeverExpires:$true
-		} else {
-		$ExistingNewAdmin = get-localuser | Where-Object {$_.Name -eq $NewAdminUsername}
-		if(!$ExistingNewAdmin){
-		write-host "Creating new user" -ForegroundColor Yellow
-		New-LocalUser -Name $NewAdminUsername -Password ($LocalAdminPassword | ConvertTo-SecureString -AsPlainText -Force) -PasswordNeverExpires:$true
-		Add-LocalGroupMember -Group Administrators -Member $NewAdminUsername
-		Disable-LocalUser -Name "Administrator"
-		}
-		else{
-			write-host "Updating admin password" -ForegroundColor Yellow
-		set-localuser -name $NewAdminUsername -Password ($LocalAdminPassword | ConvertTo-SecureString -AsPlainText -Force)
-		}
-		}
-		if($ChangeAdminUsername -eq $false ) { $username = "Administrator" } else { $Username = $NewAdminUsername }
+    #Check if there is only 1 matching asset
+    if (($ParentAsset | measure-object).count -ne 1) {
+        $ParentAsset = Get-HuduAssets -companyid $company.id -name $ComputerName
+    }
+
+    if (($ParentAsset | measure-object).count -eq 1) {
+
+        add-type -AssemblyName System.Web
+        #This is the process we'll be perfoming to set the admin account.
+        $LocalAdminPassword = [System.Web.Security.Membership]::GeneratePassword(24, 5)
+        If ($ChangeAdminUsername -eq $false) {
+            Set-LocalUser -name "Administrator" -Password ($LocalAdminPassword | ConvertTo-SecureString -AsPlainText -Force) -PasswordNeverExpires:$true
+        } else {
+            $ExistingNewAdmin = get-localuser | Where-Object { $_.Name -eq $NewAdminUsername }
+            if (!$ExistingNewAdmin) {
+                write-host "Creating new user" -ForegroundColor Yellow
+                New-LocalUser -Name $NewAdminUsername -Password ($LocalAdminPassword | ConvertTo-SecureString -AsPlainText -Force) -PasswordNeverExpires:$true
+                Add-LocalGroupMember -Group Administrators -Member $NewAdminUsername
+                Disable-LocalUser -Name "Administrator"
+            } else {
+                write-host "Updating admin password" -ForegroundColor Yellow
+                set-localuser -name $NewAdminUsername -Password ($LocalAdminPassword | ConvertTo-SecureString -AsPlainText -Force)
+            }
+        }
+        if ($ChangeAdminUsername -eq $false ) { $username = "Administrator" } else { $Username = $NewAdminUsername }
 
 
-
-		$PasswordObjectName = "$($Env:COMPUTERNAME) - Local Administrator Account"
-		$notes = "Local Admin Password for $($Env:COMPUTERNAME)"
-		# See if a password already exists
-		$password = Get-HuduPasswords -name $PasswordObjectName -companyid $company.id 
+        $PasswordObjectName = "$($Env:COMPUTERNAME) - Local Administrator Account"
+        $notes = "Local Admin Password for $($Env:COMPUTERNAME)"
+        # See if a password already exists
+        $password = Get-HuduPasswords -name $PasswordObjectName -companyid $company.id 
 	
-		if ($password) {
-			Write-Host "Updated Password"
-			$password = set-hudupassword -id $password.id -company_id $company.id -passwordable_type "Asset" -passwordable_id $ParentAsset.id -in_portal $false -password $LocalAdminPassword -description $notes -name $PasswordObjectName -username $username
-		} else {
-			Write-Host "Created Password"
-			$password = new-hudupassword -company_id $company.id -passwordable_type "Asset" -passwordable_id $ParentAsset.id -in_portal $false -password $LocalAdminPassword -description $notes -name $PasswordObjectName -username $username
-		}
+        if ($password) {
+            Write-Host "Updated Password"
+            $password = set-hudupassword -id $password.id -company_id $company.id -passwordable_type "Asset" -passwordable_id $ParentAsset.id -in_portal $false -password $LocalAdminPassword -description $notes -name $PasswordObjectName -username $username
+        } else {
+            Write-Host "Created Password"
+            $password = new-hudupassword -company_id $company.id -passwordable_type "Asset" -passwordable_id $ParentAsset.id -in_portal $false -password $LocalAdminPassword -description $notes -name $PasswordObjectName -username $username
+        }
 
 
+    } else {
+        Write-Error "A Matching Asset Could not be found"
+    }
 
 } else {
-	Write-Host "$CompanyName was not found in Hudu"
+    Write-Error "$CompanyName was not found in Hudu"
 }
